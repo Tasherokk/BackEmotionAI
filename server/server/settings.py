5f1445e14a782e923aeed520fed2d84d9b69c31d
Saduakhas Olzhas <123558553+Tasherokk@users.noreply.github.com>
2026-01-21 15:08:46 +0500

    increase jwt access lifetime

--- server/server/settings.py
+++ server/server/settings.py
@@ -145,7 +145,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),  # Увеличено с 5 минут до 1 часа
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
