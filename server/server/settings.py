c95a937e4f3c67efc72b7add39a3b84698bd9ab5
Saduakhas Olzhas <123558553+Tasherokk@users.noreply.github.com>
2026-01-22 22:40:55 +0500

    feat: add face recognition

--- server/server/settings.py
+++ server/server/settings.py
@@ -164,7 +164,6 @@ SPECTACULAR_SETTINGS = {
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
     "COMPONENT_SPLIT_REQUEST": True,
-    "SECURITY": [{"bearerAuth": []}],
     "APPEND_COMPONENTS": {
         "securitySchemes": {
             "bearerAuth": {
