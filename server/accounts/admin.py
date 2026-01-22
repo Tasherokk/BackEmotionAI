1cd326f63b5103365436efc9f23fa25ca37948c5
Saduakhas Olzhas <123558553+Tasherokk@users.noreply.github.com>
2026-01-22 22:36:21 +0500

    feat: add face recognition

--- server/accounts/admin.py
+++ server/accounts/admin.py
@@ -11,7 +11,7 @@ class UserAdmin(DjangoUserAdmin):
 
     fieldsets = (
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("name","role", "company", "department")}),
+        (_("Personal info"), {"fields": ("name", "photo", "role", "company", "department")}),
         (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
@@ -19,7 +19,7 @@ class UserAdmin(DjangoUserAdmin):
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
-            "fields": ("username", "name", "role", "company", "department", "password1", "password2", "is_staff", "is_superuser"),
+            "fields": ("username", "name", "photo", "role", "company", "department", "password1", "password2", "is_staff", "is_superuser"),
         }),
     )
 
