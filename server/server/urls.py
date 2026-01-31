e0e51a5e1edf2507ce932af42c10809385f86c49
Saduakhas Olzhas <123558553+Tasherokk@users.noreply.github.com>
2026-02-01 03:59:23 +0500

    upd: changed media sharing

--- server/server/urls.py
+++ server/server/urls.py
@@ -14,5 +14,4 @@ urlpatterns = [
     path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
