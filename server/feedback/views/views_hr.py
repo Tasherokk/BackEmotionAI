52500ff561828827f43b4a32f9fabd0edb0a9953
Saduakhas Olzhas <123558553+Tasherokk@users.noreply.github.com>
2026-02-01 02:32:04 +0500

    upd: improve structure of project

--- server/feedback/views/views_hr.py
+++ server/feedback/views/views_hr.py
@@ -3,11 +3,12 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from drf_spectacular.utils import extend_schema, OpenApiResponse
+from permissions import IsHR
 
 
 class CompanyEmployeesView(APIView):
     """Список сотрудников компании (только для HR)"""
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsHR]
 
     @extend_schema(
         responses={
@@ -35,18 +36,12 @@ class CompanyEmployeesView(APIView):
         summary="Get company employees (HR only)"
     )
     def get(self, request):
-
-        if request.user.role != 'hr':
-            return Response(
-                {"detail": "Only HR can access this endpoint"},
-                status=status.HTTP_403_FORBIDDEN
-            )
-        
         from accounts.models import User
+
         employees = User.objects.filter(
             company=request.user.company,
             role=User.Role.EMPLOYEE
         ).select_related('department').order_by('name')
         
-        from serializers import EmployeeSerializer
+        from ..serializers.serializers_hr import EmployeeSerializer
         return Response(EmployeeSerializer(employees, many=True).data)
