de136026f0d5fc95cde4bccfef79bce7029b4399
Saduakhas Olzhas <123558553+Tasherokk@users.noreply.github.com>
2026-02-21 13:29:10 +0500

    upd: add websocket to improve chat in request

--- server/request/websocket/routing.py
+++ server/request/websocket/routing.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
-from websocket import consumers
+from . import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/chat/(?P<request_id>\d+)/$", consumers.ChatConsumer.as_asgi()),
